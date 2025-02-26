3790c381d45b9512df2588b3a6f9da604f25619e
Patrick Doyle <patrick.doyle.coding@gmail.com>
2025-02-25 20:11:31 -0500

    update code

--- omega-corp-frontend/src/app/contact-us/contact-us.component.ts
+++ omega-corp-frontend/src/app/contact-us/contact-us.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core'
-import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-contact-us',
-  imports: [FormsModule],
+  imports: [],
   templateUrl: './contact-us.component.html',
-  standalone: true,
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
-  name: string = ''
-  email: string = ''
-  message: string = ''
-
-  onSubmit() {
-    console.log('Form submitted:', this.name, this.email, this.message);
-  }
 }
