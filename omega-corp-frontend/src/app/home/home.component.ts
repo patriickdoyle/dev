cab67d765962b93d76f9457bd416e0b2be13452e
Patrick Doyle <patrick.doyle.coding@gmail.com>
2025-02-25 22:37:22 -0500

    add images

--- omega-corp-frontend/src/app/home/home.component.ts
+++ omega-corp-frontend/src/app/home/home.component.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone: true
 })
 export class HomeComponent {
+  photos = [
+    "assets/omega-corp-images/1.jpg",
+    "assets/omega-corp-images/2.jpg",
+    "assets/omega-corp-images/3.jpg",
+    "assets/omega-corp-images/4.jpg",
+    "assets/omega-corp-images/5.JPG",
+    "assets/omega-corp-images/6.jpg",
+    "assets/omega-corp-images/7.jpg",
+    "assets/omega-corp-images/8.jpg",
+    "assets/omega-corp-images/9.jpg",
+    "assets/omega-corp-images/10.jpg",
+    "assets/omega-corp-images/11.jpg",
+    "assets/omega-corp-images/12.jpg",
+    "assets/omega-corp-images/13.jpg",
+    "assets/omega-corp-images/14.jpg",
+    "assets/omega-corp-images/15.jpg",
+    "assets/omega-corp-images/16.jpg",
+    "assets/omega-corp-images/17.jpg",
+    "assets/omega-corp-images/18.jpg",
+    "assets/omega-corp-images/19.jpg",
+    "assets/omega-corp-images/20.jpg",
+    "assets/omega-corp-images/21.jpg",
+    "assets/omega-corp-images/22.jpg",
+    "assets/omega-corp-images/23.jpg",
+    "assets/omega-corp-images/24.JPG",
+    "assets/omega-corp-images/25.jpg",
+    "assets/omega-corp-images/26.jpg",
+    "assets/omega-corp-images/27.jpg",
+    "assets/omega-corp-images/28.jpg",
+    "assets/omega-corp-images/29.jpg",
+    "assets/omega-corp-images/30.jpg"
+  ];
 
 }
