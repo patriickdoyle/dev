e0c701ebf2f3d4e8b9879014a0f1fe087a2c08ef
Patrick Doyle <patrick.doyle.coding@gmail.com>
2025-03-08 17:57:59 -0500

    add logic to hide/display dif photo groups onClick + refactor

---
+++ omega-corp-frontend/src/app/photos/photos.ts
@@ -0,0 +1,118 @@
+
+export const highland_photos = [
+  "Highland Project",
+  "assets/omega-corp-images/highland-ave.jpg",
+]
+export const marywatersford_photos = [
+  "Marywatersford Project",
+  "assets/omega-corp-images/marywatersford-road.jpg",
+]
+export const rockland_photos = [
+  "Rockland Project",
+  "assets/omega-corp-images/rockland-ave.jpg",
+]
+export const levering124_photos = [
+  "Levering Project 1",
+  "assets/omega-corp-images/levering-124-after-kitchen.jpg",
+  "assets/omega-corp-images/levering-124-before.JPG",
+  "assets/omega-corp-images/levering-124-beforeandafter.JPG",
+]
+export const levering132_photos = [
+  "Levering Project 2",
+  "assets/omega-corp-images/levering-132-after.jpg",
+  "assets/omega-corp-images/levering-132-after-inside.jpg",
+  "assets/omega-corp-images/levering-132-before.jpg",
+]
+export const silverwood_photos = [
+  "Silverwood Project",
+  "assets/omega-corp-images/silverwood-back.jpg",
+  "assets/omega-corp-images/silverwood.jpg",
+]
+export const pittsburgh_bridge_photos = [
+  "Pittsburgh Bridge Project",
+  "assets/omega-corp-images/pittsburgh-bridge.jpg",
+]
+export const brunswick_photos = [
+  "Brunswick Project",
+  "assets/omega-corp-images/brunswick.jpg",
+]
+export const carroll_street_photos = [
+  "Carroll Project",
+  "assets/omega-corp-images/carroll-street.jpg",
+]
+export const caton_electric_photos = [
+  "Caton Project",
+  "assets/omega-corp-images/caton-electric.jpg",
+]
+export const clarendon_road_photos = [
+  "Clarendon Project",
+  "assets/omega-corp-images/clarendon-road.jpg",
+]
+export const clinton_park_photos = [
+  "Clinton Park Project",
+  "assets/omega-corp-images/clinton-park.jpg",
+]
+export const dover_airforce_electric_photos = [
+  "Dover Airforce Project",
+  "assets/omega-corp-images/dover-airforce-electric.jpg",
+]
+export const harrison_woods_sidewalk_photos = [
+  "Harrison Woods Project",
+  "assets/omega-corp-images/harrison-woods-sidewalk.jpg",
+]
+export const kiddos_photos = [
+  "Kiddos Project",
+  "assets/omega-corp-images/kiddos-before-entrance.jpg",
+  "assets/omega-corp-images/kiddos-before-door.jpg",
+  "assets/omega-corp-images/kiddos-before-inside.jpg",
+  "assets/omega-corp-images/kiddos-bar.jpg",
+  "assets/omega-corp-images/kiddos-after-door.jpg",
+  "assets/omega-corp-images/kiddos-after-windows.jpg",
+]
+export const roma_park_masonry_photos = [
+  "Roma Park Project",
+  "assets/omega-corp-images/roma-park-masonry.jpg",
+]
+export const trenton_airforce_photos = [
+  "Trenton Airforce Project",
+  "assets/omega-corp-images/trenton-airforce.jpg",
+]
+export const boone_street_photos = [
+  "Boone Street Project",
+  "assets/omega-corp-images/boone-street-after.jpg",
+]
+export const clayton_sidewalk_photos = [
+  "Clayton Sidewalk Project",
+  "assets/omega-corp-images/clayton-sidewalk.jpg",
+]
+
+export const renovation_photos: string[][] = [
+  boone_street_photos,
+  brunswick_photos,
+  carroll_street_photos,
+  clarendon_road_photos,
+  clinton_park_photos,
+  highland_photos,
+  kiddos_photos,
+  levering124_photos,
+  levering132_photos,
+  marywatersford_photos,
+  rockland_photos,
+  silverwood_photos
+]
+
+
+export const electric_photos: string[][] = [
+  caton_electric_photos,
+  dover_airforce_electric_photos,
+
+]
+
+export const concrete_photos: string[][] = [
+  clayton_sidewalk_photos,
+  harrison_woods_sidewalk_photos,
+  pittsburgh_bridge_photos,
+  roma_park_masonry_photos,
+  trenton_airforce_photos
+]
+
